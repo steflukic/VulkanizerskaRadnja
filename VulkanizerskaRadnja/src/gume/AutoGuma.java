9162ff086ef034bc49ce3733a9edb413e2f70e62
steflukic <stef.lukic997@gmail.com>
2018-03-26 21:08:40 +0200

    Napisana je JavaDoc dokumentacija za klasu AutoGuma, za sve njenje atribute i metode.

--- VulkanizerskaRadnja/src/gume/AutoGuma.java
+++ VulkanizerskaRadnja/src/gume/AutoGuma.java
@@ -1,25 +1,55 @@
 package gume;
 
+/**
+ *@author Stefan Lukic
+ *@version 1.0
+ **/
 public class AutoGuma {
+
+	/**
+	 * Atribut marka predstavlja marku i model gume, dok se atributi precnik, sirina i visina odnose na date
+	 * realne parametre koje automobilska guma sadrzi */
 	private String markaModel = null;
 	private int precnik = 0;
 	private int sirina = 0;
 	private int visina = 0;
 
+	/**
+	 * Metoda getMarkaModel() cija je funkcija da vrati String vrednost o marki i modelu date gume
+	 * dakle vrednost koju vraca je @return String naziv modela i marke
+	 **/
 	public String getMarkaModel() {
 		return markaModel;
 	}
 
+	/**
+	 * Metoda setMarkaModel()
+	 * sadrzi @param markaModel koji predstavlja marku i model gume
+	 * koji se cuva u atributu markaModel
+	 * ukoliko se unese null vrednost ili String koji ima manje od 3 karaktera
+	 * metoda baca izuzuzetak @throws RuntimeException sa odgovarajucom porukom
+	 * */
 	public void setMarkaModel(String markaModel) {
 		if (markaModel == null || markaModel.length() < 3)
 			throw new RuntimeException("Morate uneti marku i model");
 		this.markaModel = markaModel;
 	}
 
+	/**
+	 * Metoda getPrecnik()
+	 * @return vraca vrednost precnika
+	 * */
 	public int getPrecnik() {
 		return precnik;
 	}
-
+	
+	/**
+	 * Metoda setPrecnik()
+	 * sadrzi @param markaModel koji predstavlja precnik gume
+	 * koji se cuva u atributu precnik
+	 * ukoliko se unese String koji ima manje od 13 karaktera i String koji ima vise od 22 karaktera
+	 * metoda baca izuzuzetak @throws RuntimeException sa odgovarajucom porukom
+	 * */
 	public void setPrecnik(int precnik) {
 		if (precnik < 13 && precnik > 22)
 			throw new RuntimeException("Precnik van opsega");
